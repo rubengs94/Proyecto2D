a5bffa4de31f3117e184a5325d8c70d3a358bf53
TORRERUBEN\ruben <eastmi94@gmail.com>
2020-12-17 14:56:05 +0100

    corregir

--- Proyecto/Assets/Scripts/ControlJuego.cs
+++ Proyecto/Assets/Scripts/ControlJuego.cs
@@ -18,7 +18,8 @@ public class ControlJuego : MonoBehaviour, IPointerClickHandler
     public enum Escenas
     {
         MenuPrincipal,
-        Nivel1
+        Nivel1,
+        Nivel2
     }
     public int monedas = 0;
     public double tiempo = 0;
