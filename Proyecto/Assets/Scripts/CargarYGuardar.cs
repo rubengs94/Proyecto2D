5a85b4b3c618ef69dda1420d53937ae55f42c2ee
TORRERUBEN\ruben <eastmi94@gmail.com>
2020-12-21 23:01:22 +0100

    guardadoBD

---
+++ Proyecto/Assets/Scripts/CargarYGuardar.cs
@@ -0,0 +1,46 @@
+﻿using LitJson;
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CargarYGuardar : MonoBehaviour
+{
+    //json
+    private JsonData itemData;
+    private String jsonText;
+    //variables de usuario
+    public InputField nombre;
+    public int monedas;
+    public double tiempo;
+
+    public void Guardar()
+    {
+        DatosUsuario settings = new DatosUsuario(nombre.text, monedas, tiempo);
+
+        itemData = JsonMapper.ToJson(settings);
+        File.WriteAllText(Application.dataPath + "/Datos.json", itemData.ToString());
+
+        Debug.Log(JsonUtility.ToJson(settings));
+
+        SqlServer prueba = new SqlServer();
+        prueba.InsertarDatos(nombre.text, monedas, tiempo);
+
+    }
+
+
+    [Serializable]
+    public class DatosUsuario
+    {
+        public string nombre;
+        public int monedas;
+        public double tiempo;
+
+       public DatosUsuario(string nombre, int monedas, double tiempo)
+        {
+            this.nombre = nombre;
+            this.monedas = monedas;
+            this.tiempo = tiempo;
+        }
+    }
+}
---
+++ Proyecto/Assets/Scripts/ControlJuego.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ControlJuego : MonoBehaviour, IPointerClickHandler
+{
+    #region PROPIEDADES
+    public AudioSource audio;
+    public Sprite imagenAudioOn;
+    public Sprite imagenAudioOff;
+    public Button BotonAudio;
+    public Escenas cargarEscena;
+    public enum Escenas
+    {
+        MenuPrincipal,
+        Nivel1,
+    }
+    #endregion
+
+    #region CambiarNivel
+    /*
+    public void CambiarNivel (int nivel)
+    {
+            switch (nivel)
+            {
+                case -1: Application.Quit(); break;
+                case 0: SceneManager.LoadScene("MenuPrincipal"); break;
+                case 1: SceneManager.LoadScene("Nivel1"); break;
+            }
+    }//cambiarnivel
+    */
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PantallaDeCarga.Instancia.CargarEscena(cargarEscena.ToString());
+    }
+    #endregion
+
+    #region Musica
+
+    private bool On = false;
+
+    public void Reproducir()
+    {
+        if (On)
+        {
+            audio.Stop();
+            On = false;
+            BotonAudio.image.sprite = imagenAudioOff;
+        }
+        else
+        {
+            audio.Play();
+            On = true;
+            BotonAudio.image.sprite = imagenAudioOn;
+        }
+    }
+
+    #endregion
+}
