091351dcb0d8dee76a614a481e8ff956f454c776
rubengs94 <72064001+rubengs94@users.noreply.github.com>
2021-01-08 10:11:47 +0100

    ControlPuntuacion enabled

---
+++ Proyecto/Assets/Scripts/ControlJuego.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using LitJson;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class ControlJuego : MonoBehaviour
+{
+    #region PROPIEDADES
+
+    private JsonData itemData;
+    private string jsonText;
+    public new AudioSource audio;
+    public Sprite imagenAudioOn;
+    public Sprite imagenAudioOff;
+    public Button BotonAudio;
+    public Image imagen;
+    private string juego;
+
+    #endregion
+
+    void Start()
+    {
+        imagen.enabled = false;
+        imagen.color = new Color32(0,0,0,0);
+    }
+
+    #region CambiarNivel
+
+    /// <summary>
+    /// pantalla negra para cargar
+    /// </summary>
+    /// <param name="escena"></param>
+    public void PantallaCarga(string escena)
+    {
+        juego = escena;
+        imagen.enabled = true;
+        imagen.color = new Color32(0,0,0,255);
+        Invoke("CargarEscena", 3f);
+
+    }
+    
+    /// <summary>
+    /// CArga de la escena
+    /// </summary>
+    private void CargarEscena()
+    {
+        SceneManager.LoadScene(juego);   
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+    #region Musica
+
+    private bool On = false;
+
+    public void Reproducir()
+    {
+        if (On)
+        {
+            audio.Stop();
+            On = false;
+            BotonAudio.image.sprite = imagenAudioOff;
+        }
+        else
+        {
+            audio.Play();
+            On = true;
+            BotonAudio.image.sprite = imagenAudioOn;
+        }
+    }
+
+    #endregion
+}
---
+++ Proyecto/Assets/Scripts/ControlPuntuacion.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlPuntuacion : MonoBehaviour
+{
+    #region PROPIEDADES
+
+    SqlServer sql;
+    private int Monedas = 20;
+    [Tooltip("Tiempo iniciar en Segundos")]
+    public int tiempoinicial;
+    [Tooltip("Escala del Tiempo del Reloj")]
+    [Range(-10.0f, 10.0f)]
+    public float escalaDeTiempo = 1;
+    private TextMesh Texto;
+    private float TiempoFrameConTiempoScale = 0f;
+    private float tiempoMostrarEnSegundos = 0F;
+    private float escalaDeTiempoInicial;
+    private bool EstaPausado = false;
+
+    #endregion
+
+    #region CARGAR Y GUARDAR
+
+    /// <summary>
+    /// Cargamos las monedas de la base de datos
+    /// </summary>
+    private void CargarMonedas()
+    {
+
+    }
+
+    public void guardarDatos()
+    {
+
+        sql = new SqlServer();
+        string [] separar = Texto.text.Split(':');
+        string minuto = separar[0];
+        string segundo = separar[1];
+
+        sql.ActualizarDatos(Monedas, minuto, segundo);
+    }
+
+
+    #endregion
+
+
+    #region TIEMPO
+
+
+    void Start()
+    {
+        //cargamos las monedas
+        CargarMonedas();
+
+        Texto = GameObject.Find("Reloj").GetComponent<TextMesh>();
+        tiempoMostrarEnSegundos = tiempoinicial;
+
+        ActualizarReloj(tiempoinicial);
+    }
+
+    void Update()
+    {
+        TiempoFrameConTiempoScale = Time.deltaTime * escalaDeTiempo;
+        tiempoMostrarEnSegundos += TiempoFrameConTiempoScale;
+        ActualizarReloj(tiempoMostrarEnSegundos);
+    }
+
+    /// <summary>
+    /// Actualiza el reloj de la pantalla
+    /// </summary>
+    /// <param name="tiempoEnSegundos"></param>
+    public void ActualizarReloj(float tiempoEnSegundos)
+    {
+        int minutos = 0;
+        int segundos = 0;
+        // int milisegundos = 0;
+        string textoDelReloj;
+
+        if (tiempoEnSegundos < 0) tiempoEnSegundos = 0;
+
+        minutos = (int)tiempoEnSegundos / 60;
+        segundos = (int)tiempoEnSegundos % 60;
+        //milisegundos = (int)tiempoEnSegundos / 1000;
+
+        textoDelReloj = minutos.ToString("00") + ":" + segundos.ToString("00"); //+ ":" + milisegundos.ToString("00");
+        Texto.text = textoDelReloj;
+    }
+
+    /// <summary>
+    /// Pausar o reanudar el tiempo
+    /// </summary>
+    public void Pausar()
+    {
+        if (!EstaPausado)
+        {
+            EstaPausado = true;
+            escalaDeTiempo = 0;
+        }
+        else
+        {
+            EstaPausado = false;
+            escalaDeTiempo = 1;
+        }
+    }
+
+    #endregion
+
+}
