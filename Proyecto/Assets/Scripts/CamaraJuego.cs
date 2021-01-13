7584cb4e0eb9a50645f6610d3e9a1c4f98e12e68
rubengs94 <72064001+rubengs94@users.noreply.github.com>
2021-01-13 09:21:24 +0100

    Resubida

---
+++ Proyecto/Assets/Scripts/CamaraJuego.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraJuego : MonoBehaviour
+{
+	public GameObject follow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float posy=follow.transform.position.y;
+		
+		transform.position=new Vector3(transform.position.x,posy,transform.position.z);
+    }
+}
---
+++ Proyecto/Assets/Scripts/PruebaMovimiento.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class PruebaMovimiento : MonoBehaviour
+{
+	public bool grounded;
+    public new AudioSource audio;
+	public float jumpPower;
+	private Rigidbody2D rb2d;
+	private Animator anim;
+	private bool jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb2d=GetComponent<Rigidbody2D>();
+		anim=GetComponent<Animator>();
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+		anim.SetBool("Grounded", grounded);
+		if(Input.GetButtonDown("Jump") && grounded)
+        {
+			audio.Play();//sonido salto
+		    jump= true;
+		}
+		
+		
+    }
+	
+	void FixedUpdate()
+	{
+		if(Input.GetKey(KeyCode.D) || Input.GetKey("right")){
+			anim.SetBool("Speed",true);
+            if(GetComponent<SpriteRenderer>().flipX==true){
+                GetComponent<SpriteRenderer>().flipX=false;
+            }
+            transform.Translate(0.05f,0,0);
+        }
+        else if(Input.GetKey(KeyCode.A) || Input.GetKey("left")){
+			anim.SetBool("Speed",true);
+            if(GetComponent<SpriteRenderer>().flipX==false){
+                GetComponent<SpriteRenderer>().flipX=true;
+            }
+            transform.Translate(-0.05f,0,0);
+        }
+        else{
+			anim.SetBool("Speed",false);
+            transform.Translate(0,0,0);
+        }
+		
+		
+		
+		
+		//salto
+		if(jump){
+			anim.SetBool("Speed",false);
+			rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+			jump=false;
+		}
+		//Debug.Log(rb2d.velocity.x);
+	}
+
+    #region RESPAWN
+
+    public void Respawn()
+    {
+        Invoke("LlamadaRespawn",1f);
+    }
+
+    /// <summary>
+    /// mueve el personaje al principio del mapa 1
+    /// </summary>
+    void LlamadaRespawn()
+    {
+        transform.position = new Vector3(-0.04f, -1.39f, 0.0f);
+    }
+
+
+    #endregion
+
+    #region coins
+
+
+
+    #endregion
+
+}
