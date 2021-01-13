82b74d5c64f8386e4c14024b1c82b4d2daa897df
rubengs94 <72064001+rubengs94@users.noreply.github.com>
2021-01-13 10:31:21 +0100

    PlataformaMovil

--- Proyecto/Assets/Scripts/CheckGround.cs
+++ Proyecto/Assets/Scripts/CheckGround.cs
@@ -17,11 +17,23 @@ public class CheckGround : MonoBehaviour
 		if(col.gameObject.tag=="Ground"){
 			player.grounded=true;
 		}
+
+        if (col.gameObject.tag == "Platform")
+        {
+            player.transform.parent = col.transform;
+            player.grounded = true;
+        }
     }
 	void OnCollisionExit2D(Collision2D col)
     {
 		if(col.gameObject.tag=="Ground"){
 			player.grounded=false;
 		}
+
+        if (col.gameObject.tag == "Platform")
+        {
+            player.transform.parent = null;
+            player.grounded = false;
+        }
     }
 }
