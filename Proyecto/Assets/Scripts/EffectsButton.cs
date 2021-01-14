28626b299fa6d02530c410643ee812e14ac774ab
rubengs94 <72064001+rubengs94@users.noreply.github.com>
2021-01-14 12:47:50 +0100

    Nivel1Ampliado

---
+++ Proyecto/Assets/Scripts/EffectsButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsButton : MonoBehaviour
+{
+
+    public GameObject piedra;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            piedra.SetActive(true);
+        }
+    }
+
+}
