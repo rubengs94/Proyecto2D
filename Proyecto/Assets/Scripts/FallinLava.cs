489558e130724899aff7b2916faedec67570b0d3
rubengs94 <72064001+rubengs94@users.noreply.github.com>
2021-01-15 00:03:33 +0100

    fixEnemy/addPrefabs

---
+++ Proyecto/Assets/Scripts/FallinLava.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallinLava : MonoBehaviour
+{
+    public float fallDelay = 0.5f;
+    public float respawnDelay = 5f;
+
+    private Rigidbody2D rb2d;
+    private Vector3 start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        start = transform.position;
+        Invoke("Respawn",3f);
+        
+    }
+
+
+    /// <summary>
+    /// Respawn del GameObject
+    /// </summary>
+    void Respawn()
+    {
+        transform.position = start;
+        rb2d.velocity = Vector3.zero;
+        Invoke("Respawn",3f);
+    }
+}
