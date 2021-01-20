06f4dcca90620177e16c64aa001f3ef9368b89f0
rubengs94 <72064001+rubengs94@users.noreply.github.com>
2021-01-20 10:43:31 +0100

    Finish

---
+++ Proyecto/Assets/Scripts/Report.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Report : MonoBehaviour
+{
+
+    #region PROPIEDADES
+
+    public InputField tituloReport;
+    public InputField textoReport;
+    SqlServer sql;
+
+    #endregion
+
+    #region ENVIO DE DATOS
+
+    public void Enviar()
+    {
+        sql = new SqlServer();
+        sql.Report(tituloReport.text, textoReport.text);
+
+        tituloReport.text = "";
+        textoReport.text = "";
+    }
+
+
+    #endregion
+
+
+}
